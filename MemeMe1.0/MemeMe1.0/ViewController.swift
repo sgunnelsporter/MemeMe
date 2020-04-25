27731526ba59bb1dc77ddb516ec36a866260dbcd
Sarah Gunnels Porter <zele.de.cuisine@gmail.com>
2020-04-25 15:09:01 -0400

    Initial Commit
    Created empty XCode project

---
+++ MemeMe1.0/MemeMe1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe1.0
+//
+//  Created by Sarah Gunnels Porter on 4/25/20.
+//  Copyright © 2020 Gunnels Porter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
