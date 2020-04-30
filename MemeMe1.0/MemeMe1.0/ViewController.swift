8f63e5df442d48f5cd651d0bca0b3a0f9d97d3b6
Sarah Gunnels Porter <zele.de.cuisine@gmail.com>
2020-04-29 20:43:31 -0400

    clean up and comments

--- MemeMe1.0/MemeMe1.0/ViewController.swift
+++ MemeMe1.0/MemeMe1.0/ViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
     
+    //MARK: View state variables
     var activeTextField: UITextField?
 
+    // MARK: View Outlet Defitions
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var memeImage: UIImageView!
     @IBOutlet weak var topText: UITextField!
     @IBOutlet weak var bottomText: UITextField!
     
+    //MARK: Default Text attributes
     let memeTextAttributes: [NSAttributedString.Key: Any] = [
         NSAttributedString.Key.strokeColor: UIColor.black,
         NSAttributedString.Key.foregroundColor: UIColor.white,
@@ -24,17 +27,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         NSAttributedString.Key.strokeWidth:  Float(3)
     ]
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
     
+    //MARK: Set-up and Release of ViewController
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // If camera is not available on device, disable the camera button
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
         
+        // Set textField defaults
         topText.delegate = self
         topText.defaultTextAttributes = memeTextAttributes
         topText.textAlignment = .center
@@ -43,12 +44,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         bottomText.defaultTextAttributes = memeTextAttributes
         bottomText.textAlignment = .center
         
+        // Set-up notification listening for keyboard actions
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        
+        // Disable notification listening for keyboard actions
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
@@ -82,8 +86,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     @objc func keyboardWillShow(_ notification:Notification) {
+        // Get keyboard display size
         let userInfo = notification.userInfo
         let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        
+        // only if the active text field is going to be covered by the keyboard shift view
         if let activeTextField = activeTextField {
 
           let bottomOfTextField = activeTextField.convert(activeTextField.bounds, to: self.view).maxY;
@@ -98,10 +105,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     @objc func keyboardWillHide(_ notification: Notification){
+        // return view back to origin
         view.frame.origin.y = 0
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // set active text field back to default state and dismiss keyboard
         self.activeTextField = nil
         textField.resignFirstResponder()
         return true
