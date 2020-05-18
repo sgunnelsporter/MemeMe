65810f4b9078cf3246fd7e151d2933add5167497
Sarah Gunnels Porter <zele.de.cuisine@gmail.com>
2020-05-18 10:21:56 -0400

    created color creater view with sliders

--- MemeMe1.0/MemeMe1.0/ColorPickerViewController.swift
+++ MemeMe1.0/MemeMe1.0/ColorPickerViewController.swift
@@ -10,12 +10,21 @@ import UIKit
 
 class ColorPickerViewController: UIViewController {
 
+    // MARK: Outlets
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    
+    // MARK: Navigation Controllers
     @IBAction func cancelPicker(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
@@ -24,10 +33,21 @@ class ColorPickerViewController: UIViewController {
         // Do Stuff!!
         self.dismiss(animated: true, completion: nil)
     }
+    
+    // MARK: Color Changer Function
+    @IBAction func chageColor() {
+        if self.redSlider == nil {
+            return
+        }
+        
+        let r : CGFloat = CGFloat(self.redSlider.value)
+        let g : CGFloat = CGFloat(self.greenSlider.value)
+        let b : CGFloat = CGFloat(self.blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    }
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
