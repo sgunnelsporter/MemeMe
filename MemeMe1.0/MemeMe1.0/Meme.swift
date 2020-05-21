66c2e010308c10f5e657a54c5a744ac903f3d034
Sarah Gunnels Porter <zele.de.cuisine@gmail.com>
2020-05-21 18:56:59 -0400

    created data modal for Meme

---
+++ MemeMe1.0/MemeMe1.0/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe1.0
+//
+//  Created by Sarah Gunnels Porter on 5/21/20.
+//  Copyright © 2020 Gunnels Porter. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var image : UIImage?
+    var topText : NSAttributedString?
+    var bottomText : NSAttributedString?
+    var memedImage : UIImage?
+}
