e390c473dc787961a9dcbe5e968f6b370eba8d69
Sarah Gunnels Porter <zele.de.cuisine@gmail.com>
2020-05-25 15:39:16 -0400

    added blank table and collection view controllers

---
+++ MemeMe1.0/MemeMe1.0/SentMemesCollectionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SharedMemesCollectionViewController.swift
+//  MemeMe1.0
+//
+//  Created by Sarah Gunnels Porter on 5/25/20.
+//  Copyright © 2020 Gunnels Porter. All rights reserved.
+//
+
+import UIKit
+
+class SentMemesCollectionViewController: UIViewController, UICollectionViewDelegate {
+    
+   override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ MemeMe1.0/MemeMe1.0/SharedMemesListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SharedMemesListViewController.swift
+//  MemeMe1.0
+//
+//  Created by Sarah Gunnels Porter on 5/25/20.
+//  Copyright © 2020 Gunnels Porter. All rights reserved.
+//
+
+import UIKit
+
+class SharedMemesListViewController: UIViewController, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
