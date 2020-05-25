0fb923492fe369816886026cc8b2b27912773411
Sarah Gunnels Porter <zele.de.cuisine@gmail.com>
2020-05-25 16:32:18 -0400

    connected create meme to + button
    Not functioning the way I wish right now.

---
+++ MemeMe1.0/MemeMe1.0/SentMemeCollectionCell.swift
@@ -0,0 +1,14 @@
+//
+//  SentMemeCollectionCell.swift
+//  MemeMe1.0
+//
+//  Created by Sarah Gunnels Porter on 5/25/20.
+//  Copyright © 2020 Gunnels Porter. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SentMemeCollectionCell : UICollectionViewCell {
+    
+}
